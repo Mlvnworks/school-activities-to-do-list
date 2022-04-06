8d563daca400877a05f1fbe50a66eb7c15c840f3
Melvin Agustin <agustinarsenio122@gmail.com>
2022-04-06 09:04:31 +0800

    Add files via upload

--- toDoStore copy.js
+++ toDoStore copy.js
@@ -2,13 +2,14 @@ let ctvts=[];
 
 const addBtn=document.querySelector('#add-btn');
 addBtn.addEventListener('click',()=>{
-const actTitle=document.querySelector('#activity-title').value;
-const actDate=document.querySelector('#passing').value;
-const actSubject=document.querySelector('#subject').value;
+    const actTitle=document.querySelector('#activity-title').value;
+    const actDate=document.querySelector('#passing').value;
+    const actSubject=document.querySelector('#subject').value;
+    const actLink=document.querySelector('#link').value;
     if(actTitle === "" || actDate=== ""|| actSubject===""){
         alert('please enter all fields');
     }else{
-    ctvts.unshift({title:actTitle,date:actDate,subject:actSubject,state:'unDone'});
+    ctvts.unshift({title:actTitle,date:actDate,subject:actSubject,link:actLink,state:'unDone'});
 
     localStorage.setItem('activities',JSON.stringify(ctvts));
     location.reload();
@@ -17,43 +18,101 @@ const actSubject=document.querySelector('#subject').value;
 function output(table){
     ctvts.forEach(activity=>{
         if(activity.state === 'unDone'){
-            const tr=document.createElement('tr');
-            tr.className='act';
-            tr.innerHTML=`
-            <td class="title">${activity.title}</td>
-            <td>${activity.date}</td>
-            <td>${activity.subject}</td>
-            <td>
-                <i class="bi bi-circle-fill not-done ndctn"></i>
-            </td>
-            <td>
-                <button class="btn btn-sm btn-warning state">Doned</button>
-                <button class="btn btn-small btn-danger d-none trash text-sm">
-                    <i class="bi bi-trash3-fill"></i>
-                </button>
-            </td>
-            `;
-            table.append(tr);
+            if(activity.link === ""){
+                const tr=document.createElement('tr');
+                tr.className='act';
+                tr.innerHTML=`
+                <td class="title">${activity.title}</td>
+                <td>${activity.date}</td>
+                <td>${activity.subject}</td>
+                <td>
+                    <i class="bi bi-circle-fill not-done ndctn"></i>
+                </td>
+                <td>
+                    no link
+                </td>
+                <td>
+                    <button class="btn btn-sm btn-warning state">Doned</button>
+                    <button class="btn btn-small btn-danger d-none trash text-sm">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                </td>
+                `;
+                table.append(tr);
+            }else{
+                const tr=document.createElement('tr');
+                tr.className='act';
+                tr.innerHTML=`
+                <td class="title">${activity.title}</td>
+                <td>${activity.date}</td>
+                <td>${activity.subject}</td>
+                <td>
+                    <i class="bi bi-circle-fill not-done ndctn"></i>
+                </td>
+                <td>
+                    <a href="${activity.link}" target="_blank" class="btn btn-primary btn-sm link-open">
+                        Open link
+                    </a>
+                </td>
+                <td>
+                    <button class="btn btn-sm btn-warning state">Doned</button>
+                    <button class="btn btn-small btn-danger d-none trash text-sm">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                </td>
+                `;
+                table.append(tr);
+            }
         }else{
-            const tr=document.createElement('tr');
-            tr.className='act';
-            tr.innerHTML=`
-            <td class="title">${activity.title}</td>
-            <td>${activity.date}</td>
-            <td>${activity.subject}</td>
-            <td>
-                <i class="bi bi-circle-fill done ndctn"></i>
-            </td>
-            <td>
-                <button class="btn btn-success state">
-                    <i class="bi bi-check2-all"></i>
-                </button>
-                <button class="btn btn-small btn-danger d-none trash text-sm">
-                    <i class="bi bi-trash3-fill"></i>
-                </button>
-            </td>
-            `;
-            table.append(tr);
+            if(activity.link===""){
+                const tr=document.createElement('tr');
+                tr.className='act';
+                tr.innerHTML=`
+                <td class="title">${activity.title}</td>
+                <td>${activity.date}</td>
+                <td>${activity.subject}</td>
+                <td>
+                    <i class="bi bi-circle-fill done ndctn"></i>
+                </td>
+                <td>
+                   no link
+                </td>
+                <td>
+                    <button class="btn btn-success state">
+                        <i class="bi bi-check2-all"></i>
+                    </button>
+                    <button class="btn btn-small btn-danger d-none trash text-sm">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                </td>
+                `;
+                table.append(tr);
+            }else{
+                const tr=document.createElement('tr');
+                tr.className='act';
+                tr.innerHTML=`
+                <td class="title">${activity.title}</td>
+                <td>${activity.date}</td>
+                <td>${activity.subject}</td>
+                <td>
+                    <i class="bi bi-circle-fill done ndctn"></i>
+                </td>
+                <td>
+                    <a href="${activity.link}" target="_blank" class="btn btn-primary btn-sm link-open">
+                        Open link
+                    </a>
+                </td>
+                <td>
+                    <button class="btn btn-success state">
+                        <i class="bi bi-check2-all"></i>
+                    </button>
+                    <button class="btn btn-small btn-danger d-none trash text-sm">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                </td>
+                `;
+                table.append(tr);
+            }
         }
     })
 }
